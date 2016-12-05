125ed3e8c8f7fda6f8b09944cc8cacbf9ed38849
0003299-xubuntuVB <gonzalo.moros.sales@gmail.com>
2016-12-05 12:27:49 +0100

    Add todo reducers and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -24,4 +24,51 @@ describe('Reducers', () => {
       expect(res).toBe(true);
     });
   });
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      }
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('shold toggle todo by id', () => {
+        var todos = [
+          {
+            id: 'a',
+            text: 'Una cosa',
+            completed: false,
+            createdAt: 12,
+            completedAt: undefined
+          },
+          {
+            id: 'b',
+            text: 'Otra cosa',
+            completed: true,
+            createdAt: 125,
+            completedAt: 245
+          }
+        ];
+        var action = {
+          type: 'TOGGLE_TODO',
+          id: 'a'
+        }
+          var res = reducers.todosReducer(df(todos), df(action));
+
+          expect(res[0].completed).toBe(true);
+          expect(res[0].completedAt).toBeA('number');
+
+          action = {
+            type: 'TOGGLE_TODO',
+            id: 'b'
+          }
+
+          res = reducers.todosReducer(df(todos), df(action));
+          expect(res[1].completed).toBe(false);
+          expect(res[1].completedAt).toBe(undefined);
+    });
+  });
 });
