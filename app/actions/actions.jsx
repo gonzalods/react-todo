30c5afc7f455b693aca31d78e06b1583dd381f54
gonzalo-xvb <gonzalo.moros.ds@gmail.com>
2016-12-06 17:18:51 +0100

    Fetch todos from firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -21,6 +21,33 @@ export var addTodo = (todo) => {
   };
 };
 
+export var startAddTodos = () => {
+  return (dispatch, getState) => {
+
+    var todosRef = firebaseRef.child('todos');
+
+    return todosRef.once('value').then((snapshot) => {
+      var todos = snapshot.val() || {};
+      var parsedTodos = [];
+
+      Object.keys(todos).forEach((todoId) => {
+        parsedTodos.push({
+          id: todoId,
+          ...todos[todoId]
+        });
+      });
+      dispatch(addTodos(parsedTodos));
+    });
+  }
+};
+
+export var addTodos = (todos) => {
+  return {
+    type: 'ADD_TODOS',
+    todos
+  };
+};
+
 export var startAddTodo = (text) => {
   return (dispatch, getState) => {
     var todo = {
@@ -37,13 +64,6 @@ export var startAddTodo = (text) => {
         id: todoRef.key
       }))
     })
-  };
-};
-
-export var addTodos = (todos) => {
-  return {
-    type: 'ADD_TODOS',
-    todos
   };
 };
 
